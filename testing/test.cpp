2ab5c7698a2341bf35160fd18293405959956fc6
shravanrn@gmail.com <shravanrn@gmail.com>
2019-10-05 00:29:57 -0700

    Some simple testing build scripts

---
+++ testing/test.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+extern "C" {
+    
+int singleBranch(int val)
+{
+    if (val) {
+        return 5;
+    } else {
+        return 7;
+    }
+}
+
+void printBranch(int val)
+{
+    if (val) {
+        printf("if side\n");
+    } else {
+        printf("else side\n");
+    }
+}
+}
+
+int main(int argc, char** argv)
+{
+    singleBranch(argc);
+}
